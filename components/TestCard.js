acd830fa6181cac1dfaf76d100354fde99d94f9f
Jonathan Dunlap <jdunlap@outlook.com>
2020-07-29 21:10:49 -0700

    slug added

--- components/TestCard.js
+++ components/TestCard.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player';
-import Link from 'next/link';
-
+import React, { useState, useEffect } from "react";
+import ReactPlayer from "react-player";
+import Link from "next/link";
 
 export default function TestCard({ tags, title, video, gif, file }) {
-
-// Sample file retrieval logic
+  // Sample file retrieval logic
 
   // const [images, setImages] = useState([]);
   // const [files, setFiles] = useState([]);
@@ -29,20 +27,19 @@ export default function TestCard({ tags, title, video, gif, file }) {
         <div className="absolute bottom-0 left-0 px-6 py-4">
           <div className="font-bold text-xl mb-2 text-gray-500">{title}</div>
 
-
-
           <div className="flex justify-between items-center py-4">
-            <Link href="/player">
+            <Link href="/post/[slug]" as={"/post/" + file.id}>
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-2">
                 3 $TINGLES
               </button>
             </Link>
           </div>
 
-          <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2">Tags: {tags}</div>
+          <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2">
+            Tags: {tags}
+          </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
