da63d7b055e9332ba520d170d1475223bb03ef77
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-08-05 00:15:16 -0400

    added styling to WallCard.js and added ProfileHeader.js

--- components/WallCard.js
+++ components/WallCard.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Icon, InlineIcon } from '@iconify/react';
 import playSolid from '@iconify/icons-la/play-solid';
+import lockSolid from '@iconify/icons-la/lock-solid';
 import heartSolid from "@iconify/icons-la/heart-solid";
 import commentsSolid from "@iconify/icons-la/comments-solid";
 import shareAltSquareSolid from "@iconify/icons-la/share-alt-square-solid";
 import baselineShare from "@iconify/icons-ic/baseline-share";
+import ProfileHeader from "./profileHeader";
 
 export default function WallCard({ tags, title, video, gif, file }) {
   // Sample file retrieval logic
@@ -30,8 +32,12 @@ export default function WallCard({ tags, title, video, gif, file }) {
       <Link href="/post/[slug]" as={"/post/" + file.id}>
         <div className="rounded shadow-lg h-full mx-auto w-auto max-w-md cursor-pointer">
           <img className="h-full w-full object-cover" src={gif} />
+          <div className="absolute top-0 p-6">
+            <ProfileHeader />
+          </div>
           <div className="absolute bottom-0 mx-6 mb-24">
-            <div className="break-words font-extrabold text-5xl mb-2 p-2 text-white mx-auto w-auto max-w-md">{title}</div>
+            <Icon className="w-10 h-10" icon={lockSolid} color="white" />
+            <div className="break-words font-extrabold text-4xl mb-4 p-2 text-white mx-auto w-auto max-w-sm">{title}</div>
             <div className="flex py-4">
               <button className="flex bg-white hover:bg-gray-300 text-black font-medium tracking-wide py-2 px-4 w-48 rounded-lg mr-2 items-center">
                 <Icon className="m-1" icon={playSolid} color="black" /> Tier 2 Members
