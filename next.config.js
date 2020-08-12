d53e61e7b2857dbe45b2bd7320569498c32f9b29
Jonathan Dunlap <jdunlap@outlook.com>
2020-08-11 21:01:46 -0700

    chore: webpack 5 fix for build and export

--- next.config.js
+++ next.config.js
@@ -5,14 +5,22 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    // config.resolve.mainFields = config.mainFields || [];
-    // config.resolve.mainFields.push("browser");
+    /*
+    if (!isServer) {
+      config.resolve.mainFields = config.resolve.mainFields || [];
+      config.resolve.mainFields.push("browser");
+
+      config.mainFields = config.mainFields || [];
+      config.mainFields.push("browser");
+    }*/
     // config.externals = config.externals || [];
     // config.externals.push("browser");
     // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
 
-    config.resolve.alias.https = "https-browserify";
-    config.resolve.alias.http = "http-browserify";
+    if (isServer || dev) {
+      config.resolve.alias.https = "https-browserify";
+      config.resolve.alias.http = "http-browserify";
+    }
 
     // Important: return the modified config
     return config;
