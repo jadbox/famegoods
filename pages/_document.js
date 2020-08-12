1b83de6b5d8c6c2537e58bfd4a07e0c6a2ebda14
Jonathan Dunlap <jdunlap@outlook.com>
2020-08-11 21:46:54 -0700

    chore: add ico and manifest

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,21 @@ class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <script>
-          if(ethereum) ethereum.autoRefreshOnNetworkChange = false;
-        </script>
-        <Head />
+        <Head>
+          <title>DFAME</title>
+          <script>
+            if(ethereum) ethereum.autoRefreshOnNetworkChange = false;
+          </script>
+          <link
+            href="https://assets.website-files.com/5f305ad8d02acc9affb63d10/5f3072d448c0a72cd77bc6cb_DFAME-Icon32px.png"
+            rel="shortcut icon"
+            type="image/x-icon"
+          />
+          <link
+            href="https://assets.website-files.com/5f305ad8d02acc9affb63d10/5f3072da856f6e19788fa9b4_DFAME-Icon256px.png"
+            rel="apple-touch-icon"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
