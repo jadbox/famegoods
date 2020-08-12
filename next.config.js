65d5af0712a1302df47b84d70500155e16c7c24f
Jonathan Dunlap <jdunlap@outlook.com>
2020-08-11 20:31:08 -0700

    chore: upgrade to webpack 5 and latest nextjs

--- next.config.js
+++ next.config.js
@@ -2,4 +2,19 @@ module.exports = {
   devIndicators: {
     autoPrerender: false,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // config.resolve.mainFields = config.mainFields || [];
+    // config.resolve.mainFields.push("browser");
+    // config.externals = config.externals || [];
+    // config.externals.push("browser");
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+
+    config.resolve.alias.https = "https-browserify";
+    config.resolve.alias.http = "http-browserify";
+
+    // Important: return the modified config
+    return config;
+  },
 };
