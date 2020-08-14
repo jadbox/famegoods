31f2729e1dc2650df4f97ab86a428ab8ca1321e0
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2020-08-14 14:03:33 -0400

    added styling back to the previous pages (#20)

---
+++ components/header.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div>
+            <nav class="bg-white pt-4 px-4 pb-2 justify-left fixed w-full z-10 top-0">
+                <div class="container mx-auto flex flex-wrap items-center">
+                    <h3
+                        style={{
+                            fontStyle: "italic",
+                            fontSize: "1.4em",
+                            fontWeight: 800,
+                            color: "black",
+                        }}
+                    >
+                        🔥 WHAT'S HOT
+          </h3>
+                </div>
+            </nav>
+            {/*
+        <header className="fixed top-0 bg-white w-full h-12">
+            <h3
+                style={{
+                    fontStyle: "italic",
+                    fontSize: "1.4em",
+                    fontWeight: 800,
+                    color: "black",
+                }}
+            >
+                🔥 WHAT'S HOT
+          </h3>
+            </header>*/}
+        </div >
+    );
+}
