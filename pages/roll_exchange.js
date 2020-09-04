92eec429210e166648f30413382d248c990fb03d
Luis <lfegea@aol.com>
2020-09-04 12:11:49 -0700

    chore: exchange iframe takes full screen available

--- pages/roll_exchange.js
+++ pages/roll_exchange.js
@@ -1,14 +1,10 @@
 const RollExchange = () => {
   return (
-    <div className="pb-24">
-      <iframe 
-        className="max-w-screen w-full min-h-screen" 
-        style={{marginTop: "-68px"}} 
-        src='https://exchange.tryroll.com/#/swap'
-      />
-    </div>
-   );
-
+    <iframe 
+      className="max-w-screen w-full min-h-screen"
+      src='https://exchange.tryroll.com/#/swap'
+    />   
+  );
 };
 
 export default RollExchange;
