f5fdfc90c40f730505af6e417da40a2523da9649
Luis <lfegea@aol.com>
2020-09-08 19:51:39 -0700

    style: formatted iframe exchange to show connected wallet at top

--- pages/roll_exchange.js
+++ pages/roll_exchange.js
@@ -1,9 +1,12 @@
 const RollExchange = () => {
   return (
-    <iframe 
-      className="max-w-screen w-full min-h-screen"
-      src='https://exchange.tryroll.com/#/swap'
-    />   
+    <div className="pb-24">
+      <iframe 
+        className="max-w-screen w-full min-h-screen" 
+        style={{marginTop: "-24px"}} 
+        src='https://exchange.tryroll.com/#/swap'
+      />
+    </div>
   );
 };
 
