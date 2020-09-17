36d69ed654e2b34d1c316bee2af2e7582295381e
Luis Fernando <lfegea@aol.com>
2020-09-17 11:30:57 -0700

    Wallet menu (#34)
    * feat: menu to buy, sell, & transfer tokens + connect wallets
    
    * fix: auth server login

---
+++ pages/roll_transfer.js
@@ -0,0 +1,12 @@
+const RollTransfer = () => {
+  return (
+    <div className="pb-24">
+      <iframe 
+        className="max-w-screen w-full min-h-screen"  
+        src='https://app.tryroll.com/send/rollWallet'
+      />
+    </div>
+  );
+};
+
+export default RollTransfer;
